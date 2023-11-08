b7f20e1106131893081599c3c12c4d4d83528c99
Chanda Gonet <chandagonet@gmail.com>
2023-11-07 19:37:08 -0500

    next config for CORS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // Specify the allowed origin(s) in place of "*"
+          { key: "Access-Control-Allow-Origin", value: "https://www.tattoochanda.com" }, // Change to your specific origin(s)
+          { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, PATCH, DELETE, POST, PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
